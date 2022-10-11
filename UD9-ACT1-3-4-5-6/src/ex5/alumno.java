9d66c92f50bad5f936b5adc1a7c69f04b1255b19
Carlos <CarlosMonteroMarin@carlosmonteromarin@gmail.com>
2022-10-11 09:51:10 +0200

    commit clases alumno y profesor

--- UD9-ACT1-3-4-5-6/src/ex5/alumno.java
+++ UD9-ACT1-3-4-5-6/src/ex5/alumno.java
@@ -1,5 +1,51 @@
 package ex5;
 
-public class alumno {
+public class alumno extends personas {
+	//Atributos
+	private int notaActual;
+	
+	
+	
+	//Constructores
+	public alumno() {
+		super(nombre, edad, sexo);
+		this.notaActual=notaActual;
+	}
+	
+	
+	public alumno(int notaActual) {
+		super(nombre, edad, sexo);
+		this.notaActual = notaActual;
+	}
 
+
+	//Getters y setters
+	public int getNotaActual() {
+		return notaActual;
+	}
+
+
+	public void setNotaActual(int notaActual) {
+		this.notaActual = notaActual;
+	}
+	
+	
+	public void asistencia() {
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- UD9-ACT1-3-4-5-6/src/ex5/profesor.java
+++ UD9-ACT1-3-4-5-6/src/ex5/profesor.java
@@ -1,5 +1,35 @@
 package ex5;
 
-public class profesor {
+public class profesor extends personas {
+	private String materia;
+
+	
+	public profesor() {
+		super(nombre, edad, sexo);
+		this.materia = materia;
+	}
+
+	
+	public profesor(String materia) {
+		super(nombre, edad, sexo);
+		this.materia = materia;
+	}
+
+	
+	public String getMateria() {
+		return materia;
+	}
+
+	
+	public void setMateria(String materia) {
+		this.materia = materia;
+	}
+	
+	
+	
+	
+	
+	
+	
 
 }
