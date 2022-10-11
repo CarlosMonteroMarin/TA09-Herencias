9fe0e826f3c81348138d45beb44ff2c2b36839e0
Victoria Geeks ♡ <victoriageeks@gmail.com>
2022-10-11 09:49:40 +0200

    clase personas

--- UD9-ACT1-3-4-5-6/src/ex5/personas.java
+++ UD9-ACT1-3-4-5-6/src/ex5/personas.java
@@ -1,5 +1,45 @@
 package ex5;
 
-public class personas {
+public abstract class personas {
+	
+	//Atributos
+	protected String nombre;
+	protected int edad;
+	protected String sexo;
+	
+	protected String SEXO_D = "hombre";
+	
+	
+	//Constructores
+	
+	//Constructor basico
+	public personas() {
+		this.nombre = "";
+		this.edad = 0;
+		this.sexo = SEXO_D;
+	}
+	
+	//Constructor con todos los atributos
+	public personas(String nombre,int edad,String sexo) {
+		this.nombre = "";
+		this.edad = 0;
+		this.sexo = sexo;
+	}
+	
+	
+	//Getters
+	public String getNombre() {
+		return nombre;
+	}
 
+	public int getEdad() {
+		return edad;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+	
+	//método asistencia
+	public abstract boolean asistencia();
 }
